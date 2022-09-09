c9638e95ac72a7d8edacb7c8072131185f98fb47
Vit0rg <vgois032@gmail.com>
2022-09-08 23:21:02 -0300

    Responsividade implementada + Easter egg

--- js/script.js
+++ js/script.js
@@ -9,49 +9,58 @@ const buttonNext = document.querySelector('.btn-next');
 
 let searchPokemon = 1;
 
-const fetchPokemon = async (pokemon) => {
+const fetchPokemon = async (pokemon) => 
+{
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-  if (APIResponse.status === 200) {
+  if (APIResponse.status === 200) 
+  {
     const data = await APIResponse.json();
     return data;
   }
 }
 
-const renderPokemon = async (pokemon) => {
+const renderPokemon = async (pokemon) => 
+{
 
   pokemonName.innerHTML = 'Loading...';
   pokemonNumber.innerHTML = '';
 
   const data = await fetchPokemon(pokemon);
 
-  if (data) {
-    pokemonImage.style.display = 'block';
+  if (data) 
+  {
+    pokemonImage.style.display = 'flex';
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
     pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
     input.value = '';
     searchPokemon = data.id;
-  } else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not found :c';
-    pokemonNumber.innerHTML = '';
+  } else 
+  {
+    pokemonImage.src = '../images/missingno.jpeg';
+    pokemonName.innerHTML = 'MissingNo';
+    pokemonNumber.innerHTML = '666';
   }
 }
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => 
+{
   event.preventDefault();
   renderPokemon(input.value.toLowerCase());
 });
 
-buttonPrev.addEventListener('click', () => {
-  if (searchPokemon > 1) {
+buttonPrev.addEventListener('click', () => 
+{
+  if (searchPokemon > 1) 
+  {
     searchPokemon -= 1;
     renderPokemon(searchPokemon);
   }
 });
 
-buttonNext.addEventListener('click', () => {
+buttonNext.addEventListener('click', () => 
+{
   searchPokemon += 1;
   renderPokemon(searchPokemon);
 });
